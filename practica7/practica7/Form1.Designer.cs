a392e694e01b4add23ff7260321a9d8a3471afe8
diegomart <judimay_18@hotmail.com>
2013-05-01 00:26:24 +0200

    realizacion de commit practica 7

---
+++ practica7/practica7/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace practica7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Terminar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calendario = new System.Windows.Forms.MonthCalendar();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioGDL = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.suma = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CHECKPERU = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.CHECKMX = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Grupo = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.suma)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Terminar);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.calendario);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioGDL);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.checkBox4);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.suma);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.CHECKPERU);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.CHECKMX);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.Grupo);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Nombre);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(481, 617);
+            this.panel1.TabIndex = 13;
+            // 
+            // Terminar
+            // 
+            this.Terminar.Location = new System.Drawing.Point(167, 583);
+            this.Terminar.Name = "Terminar";
+            this.Terminar.Size = new System.Drawing.Size(75, 23);
+            this.Terminar.TabIndex = 33;
+            this.Terminar.Text = "Terminar";
+            this.Terminar.UseVisualStyleBackColor = true;
+            this.Terminar.Click += new System.EventHandler(this.Terminar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(249, 13);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "4-¿Indica la fecha de la independencia de México?";
+            // 
+            // calendario
+            // 
+            this.calendario.Location = new System.Drawing.Point(34, 397);
+            this.calendario.Name = "calendario";
+            this.calendario.TabIndex = 31;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(216, 296);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(58, 17);
+            this.radioButton4.TabIndex = 30;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Tonalá";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(125, 296);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 17);
+            this.radioButton3.TabIndex = 29;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Zapopan";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioGDL
+            // 
+            this.radioGDL.AutoSize = true;
+            this.radioGDL.Location = new System.Drawing.Point(317, 296);
+            this.radioGDL.Name = "radioGDL";
+            this.radioGDL.Size = new System.Drawing.Size(82, 17);
+            this.radioGDL.TabIndex = 28;
+            this.radioGDL.TabStop = true;
+            this.radioGDL.Text = "Guadalajara";
+            this.radioGDL.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(34, 296);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 27;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Jalisco";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(322, 161);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(55, 17);
+            this.checkBox4.TabIndex = 26;
+            this.checkBox4.Text = "Japón";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "3.- ¿Cual es la capital de Jalisco?";
+            // 
+            // suma
+            // 
+            this.suma.Location = new System.Drawing.Point(34, 230);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(47, 20);
+            this.suma.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "2- ¿Cuanto es: 4*5+1-8?";
+            // 
+            // CHECKPERU
+            // 
+            this.CHECKPERU.AutoSize = true;
+            this.CHECKPERU.Location = new System.Drawing.Point(132, 161);
+            this.CHECKPERU.Name = "CHECKPERU";
+            this.CHECKPERU.Size = new System.Drawing.Size(48, 17);
+            this.CHECKPERU.TabIndex = 22;
+            this.CHECKPERU.Text = "Peru";
+            this.CHECKPERU.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(231, 161);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(69, 17);
+            this.checkBox2.TabIndex = 21;
+            this.checkBox2.Text = "Alemania";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // CHECKMX
+            // 
+            this.CHECKMX.AutoSize = true;
+            this.CHECKMX.Location = new System.Drawing.Point(34, 161);
+            this.CHECKMX.Name = "CHECKMX";
+            this.CHECKMX.Size = new System.Drawing.Size(60, 17);
+            this.CHECKMX.TabIndex = 20;
+            this.CHECKMX.Text = "México";
+            this.CHECKMX.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Codigo";
+            // 
+            // Grupo
+            // 
+            this.Grupo.FormattingEnabled = true;
+            this.Grupo.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D"});
+            this.Grupo.Location = new System.Drawing.Point(358, 25);
+            this.Grupo.Name = "Grupo";
+            this.Grupo.Size = new System.Drawing.Size(44, 21);
+            this.Grupo.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(72, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(317, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Grupo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "1.- ¿Cuales son los paises de America?";
+            // 
+            // Nombre
+            // 
+            this.Nombre.Location = new System.Drawing.Point(72, 26);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(239, 20);
+            this.Nombre.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Nombre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 619);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.suma)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Terminar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MonthCalendar calendario;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioGDL;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown suma;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox CHECKPERU;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox CHECKMX;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox Grupo;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Nombre;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ practica7/practica7/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace practica7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Terminar_Click(object sender, EventArgs e)
+        {
+
+            int puntos = 0;
+
+            if (this.radioGDL.Checked)
+                puntos++;
+
+            if (this.suma.Text == "13")
+                puntos++;
+
+            if (this.CHECKMX.Checked)
+                puntos++;
+            if (this.CHECKPERU.Checked)
+                puntos++;
+
+
+
+            string fechaseleccionado = calendario.SelectionStart.ToString();
+            if (fechaseleccionado == "16/04/2013 12:00:00 a. m.")
+                puntos++;
+
+
+            MessageBox.Show("Aciertos " + puntos, "Putos");
+          
+
+        }
+
+      
+
+      
+
+      
+    }
+}
